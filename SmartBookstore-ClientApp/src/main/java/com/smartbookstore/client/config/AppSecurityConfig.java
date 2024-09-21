9de55b221448e7ee40ceffc0241b51fb63866e25
Ghifary <masterghifary6403@gmail.com>
2024-09-21 11:39:24 +0700

    add project

---
+++ SmartBookstore-ClientApp/src/main/java/com/smartbookstore/client/config/AppSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.smartbookstore.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    throws Exception {
+    return http.authorizeRequests(auth -> auth
+          .antMatchers("/css/**", "/img/**", "/js/**")
+          .permitAll()
+          .antMatchers("/login/**")
+          .permitAll()
+          .antMatchers("/register/**")
+          .permitAll()
+          .antMatchers("/dashboard/**").hasRole("ADMIN")
+          .antMatchers("/transaction/**").hasRole("ADMIN")
+          .antMatchers("/usermanagement/**").hasRole("ADMIN")
+          .antMatchers("/book").hasRole("ADMIN")
+          .antMatchers("/book/create").hasRole("ADMIN")
+          .antMatchers("/book/update/**").hasRole("ADMIN")
+          .antMatchers("/book/order/**").hasRole("USER")
+          .antMatchers("/category/**").hasRole("ADMIN")
+          .antMatchers("/author/**").hasRole("ADMIN")
+          .antMatchers("/publisher/**").hasRole("ADMIN")
+          .antMatchers("/usermanagement/**").hasRole("ADMIN")
+          .anyRequest()
+          .authenticated()
+      )
+      .formLogin(login -> login
+          .loginPage("/login")
+          .loginProcessingUrl("/login")
+          .successForwardUrl("/home")
+          .failureForwardUrl("/login?error=true")
+          .permitAll()
+      )
+      .logout(logout -> logout.logoutUrl("/logout").permitAll())
+      .build();
+  }
+}
